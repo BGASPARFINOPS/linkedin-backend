b70707c670faa2a20a5b23ff2e547276ec5744ca
BGASPARFINOPS <orgbgspr@gmail.com>
2026-03-24 02:17:38 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,100 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+let userToken = null;
+let personUrn = null;
+
+app.get("/", (req, res) => {
+  res.send("Backend funcionando");
+});
+
+app.get("/login", (req, res) => {
+  const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+    REDIRECT_URI
+  )}&scope=openid%20profile%20w_member_social`;
+
+  res.redirect(url);
+});
+
+app.get("/callback", async (req, res) => {
+  const code = req.query.code;
+
+  const tokenRes = await fetch(
+    "https://www.linkedin.com/oauth/v2/accessToken",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        code,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        redirect_uri: REDIRECT_URI,
+      }),
+    }
+  );
+
+  const tokenData = await tokenRes.json();
+  userToken = tokenData.access_token;
+
+  const userRes = await fetch(
+    "https://api.linkedin.com/v2/userinfo",
+    {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    }
+  );
+
+  const userData = await userRes.json();
+  personUrn = `urn:li:person:${userData.sub}`;
+
+  res.send("LinkedIn conectado correctamente");
+});
+
+app.post("/post", async (req, res) => {
+  const { text } = req.body;
+
+  const response = await fetch(
+    "https://api.linkedin.com/v2/ugcPosts",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+        "X-Restli-Protocol-Version": "2.0.0",
+      },
+      body: JSON.stringify({
+        author: personUrn,
+        lifecycleState: "PUBLISHED",
+        specificContent: {
+          "com.linkedin.ugc.ShareContent": {
+            shareCommentary: { text },
+            shareMediaCategory: "NONE",
+          },
+        },
+        visibility: {
+          "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC",
+        },
+      }),
+    }
+  );
+
+  const data = await response.text();
+  res.send(data);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor listo");
+});
